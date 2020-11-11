eeb964a1c6bdb39591f268fc83872113da6066f5
celinechoi <web.jinseul@gmail.com>
2020-11-11 13:19:04 +0900

    브랜치 하나로

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+export const css = {
+  loaderOptions: {
+    sass: {
+      additionalData: `
+          @import "~@/assets/styles/_mixins.scss";
+          @import "~@/assets/styles/_variables.scss";
+        `,
+    },
+  },
+};
