c4e53171c2b14808a51ee37dd02381dc617fcac8
celinechoi <web.jinseul@gmail.com>
2020-11-11 17:31:02 +0900

    sass 셋팅 및 구조 세팅

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SafeHeader></SafeHeader>
+    <SafeList></SafeList>
+    <SafeFooter></SafeFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import SafeHeader from './components/SafeHeader';
+import SafeList from './components/SafeList';
+import SafeFooter from './components/SafeFooter';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    SafeHeader,
+    SafeList,
+    SafeFooter,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import '~@/assets/styles/reset.css';
+@import '~@/assets/styles/styles.scss';
 </style>
---
+++ src/components/SafeHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="header">
+    <a href="javascript:;">전국 안심식당 리스트 조회 프로젝트</a>
+    <div class="search-box">
+      <input type="text" class="search-box__input" />
+      <a href="javascript:;" class="search-box__button">검색</a>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
