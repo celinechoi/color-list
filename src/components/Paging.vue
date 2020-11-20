029c9cd9a368a69f2b308bd78e4fcd4d104132e0
celinechoi <web.jinseul@gmail.com>
2020-11-20 11:26:31 +0900

    페이징 스타일 : 해당 페이지일때 숫자 on

--- src/components/Paging.vue
+++ src/components/Paging.vue
@@ -4,7 +4,7 @@
 			class="frame-contents__page"
 			v-for="(page, idx) in totalPage"
 			:key="idx"
-			@click="movePage(page)"
+			@click="movePage(page, idx)"
 		>
 			{{ page }}
 		</li>
@@ -19,10 +19,20 @@ export default {
 		return {};
 	},
 	methods: {
-		movePage(page) {
+		movePage(page, idx) {
 			// 이벤트 부모 컴포넌트로 올리기
 			this.$emit('movePage', page);
-			console.log(page);
+			console.log('page', page, 'idx', idx);
+			const list = document.querySelectorAll('.frame-contents__page'),
+				$colorBlack = '#000',
+				$colorGray04 = 'rgba(0, 0, 0, 0.4)';
+			for (var i = 0; i < list.length; i++) {
+				if (idx == i) {
+					list[i].style.color = $colorBlack;
+				} else {
+					list[i].style.color = $colorGray04;
+				}
+			}
 		},
 	},
 };
